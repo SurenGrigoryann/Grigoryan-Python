db076ad532ce1f3066c67afd4df74d7ae3d8b6be
SurenGrigoryann <144111293+SurenGrigoryann@users.noreply.github.com>
2023-09-12 15:28:32 +0100

    Update bowling.py

--- classwork/bowling.py
+++ classwork/bowling.py
@@ -7,19 +7,25 @@ strike_twice = 0
 
 while True:
     bowling_balls += 1
+    # counting how many times our user played the game
     set_one = input('your first bowling score > ')
+    # trying if our user have a strike from first shot. If it is so we should give him double points every time he scores.
     if set_one == '10':
         if strike_once == 1:
-            score = score + 20    
+            
+            score = score + 20  
+        
         else:
             score += 10
             strike_once = 1
+            
+        # end if
         print('Wow strike!')
         strike = 1
         print(f'Your score is {score}')
-        pass
+        continue
+    # end if
     else:
-
         set_two = input('your second bowling score > ')
         if strike_twice != 0 or strike_once != 0:
             score += (int(set_one) + int(set_two))*2
